8a254a060f541be7807cd93defa60e9402173ae7
Ibrahim <aitkazindev@gmail.com>
2024-09-25 19:16:43 +0900

    Initial Commit

---
+++ ShoppingTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingTCA
+//
+//  Created by Ibrahim Aitkazin on 25.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingTCA/ShoppingTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingTCAApp.swift
+//  ShoppingTCA
+//
+//  Created by Ibrahim Aitkazin on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
